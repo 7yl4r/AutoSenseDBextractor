71f4a46980834d67ed6e2167f765e0367be55791
Tylar <murray.tylar@gmail.com>
2012-08-05 18:01:49 -0400

    fixing organization attempt

--- mainExporter.py
+++ mainExporter.py
@@ -8,7 +8,7 @@ import model_exporter
 import feature_exporter
 
 #set up sqlite db stream
-FILENAME="./StressInferencePhone_7thApril_Demo_For_Commercial_Appeal.db"
+FILENAME="db/StressInferencePhone_7thApril_Demo_For_Commercial_Appeal.db"
 conn = sqlite3.connect(FILENAME)	#connection
 c = conn.cursor()	#cursor
 
@@ -25,6 +25,6 @@ otherTables = ['EMA_metadata',
 	'incentives'
 	]
 
-sensor_exporter.exportSensors(c,"sensorsOut.csv")
-model_exporter.exportModels(c,"modelsOut.csv")
-feature_exporter.exportFeatures(c,"featuresOut.csv")
+sensor_exporter.exportSensors(c,"output/sensorsOut.csv")
+model_exporter.exportModels(c,"output/modelsOut.csv")
+feature_exporter.exportFeatures(c,"output/featuresOut.csv")
